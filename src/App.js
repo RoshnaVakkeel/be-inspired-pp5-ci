5c9694066c217e7e34f0961ba4e796a2581f8bd6
RoshnaVakkeel <vakkeel.roshna@gmail.com>
2023-02-20 11:59:28 +0000

    Install React Router library and set up routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
 import Container from 'react-bootstrap/Container';
+import { Route,Switch } from 'react-router-dom';
 
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
-      <Container>
-        <h2>Home Page</h2>
-        <h2>Sign In</h2>
-        <h2>Sign Up</h2>
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={() => <h2>Home Page</h2>} />
+          <Route exact path="/signup" render={() => <h2>Sign Up</h2>} />
+          <Route exact path="/signin" render={() => <h2>Sign in</h2>} />
+          <Route render={() => <h2>Page not found</h2>} />
+        </Switch>
       </Container>
     </div>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,23 +1,37 @@
 import React from 'react'
 import {
-    Container,
-    Navbar,
-    Nav,
-  } from 'react-bootstrap';
+  Container,
+  Navbar,
+  Nav,
+} from 'react-bootstrap';
 import styles from "../styles/NavBar.module.css"
+import { NavLink } from "react-router-dom";
 
 
 const NavBar = () => {
   return (
-    <Navbar bg="warning" className="shadow p-3 mb-2 rounded"  expand="md" fixed="top">
+    <Navbar bg="warning" className="shadow p-3 mb-2 rounded" expand="md" fixed="top">
       <Container className="p-2">
-        <Navbar.Brand className={styles.NavBarBrand}href="#home">"Be Inspired"</Navbar.Brand>
+        <NavLink to="/">
+          <Navbar.Brand className={styles.NavBarBrand} href="#home">
+            "Be Inspired"
+          </Navbar.Brand>
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse  id="basic-navbar-nav" className={styles.NavItems}>
+        <Navbar.Collapse id="basic-navbar-nav" className={styles.NavItems}>
           <Nav className="ml-auto text-left">
-            <Nav.Link href="#home"><i class="fa fa-home" aria-hidden="true"></i> Home</Nav.Link>
-            <Nav.Link href="#link"><i class="fa fa-user-plus" aria-hidden="true"></i>  Sign Up</Nav.Link>
-            <Nav.Link href="#link"><i class="fa fa-sign-in-alt" aria-hidden="true"></i>  Sign In</Nav.Link>
+            <NavLink exact className={styles.NavLink} activeClassName={styles.Active} to="/" href="#home">
+              <i class="fa fa-home" aria-hidden="true"></i>
+              Home
+            </NavLink>
+            <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signup" href="#link">
+              <i class="fa fa-user-plus" aria-hidden="true"></i>
+              Sign Up
+            </NavLink>
+            <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signin" href="#link">
+              <i class="fa fa-sign-in-alt" aria-hidden="true"></i>
+              Sign In
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
