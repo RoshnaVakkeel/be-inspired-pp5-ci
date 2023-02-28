767da15e7b6fc3f66d6dd2874ddd281f8865ba2e
RoshnaVakkeel <vakkeel.roshna@gmail.com>
2023-02-28 23:55:13 +0000

    Route PostEditForm page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import SignUpForm from './pages/auth/SignUpForm';
 import SignInForm from './pages/auth/SignInForm';
 import PostCreateForm from './pages/posts/PostCreateForm';
 import PostPage from './pages/posts/PostPage';
+import PostEditForm from './pages/posts/PostEditForm';
 
 import { useCurrentUser } from './contexts/CurrentUserContext';
 import PostsListPage from './pages/posts/PostsListPage';
@@ -51,6 +52,7 @@ function App() {
           <Route exact path="/signin" render={() => <SignInForm />} />
 
           <Route exact path="/posts/create" render={() => <PostCreateForm />} />
+          <Route exact path="/posts/:id/edit" render={() => <PostEditForm />} />
           <Route exact path="/posts/:id" render={() => <PostPage />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
