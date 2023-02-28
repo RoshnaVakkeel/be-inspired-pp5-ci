87e1b905501df0fa2f6ec3c2735192bd09254a59
RoshnaVakkeel <vakkeel.roshna@gmail.com>
2023-02-28 10:52:35 +0000

    Add boilerplate code for PostsPage/Home

---
+++ src/pages/posts/PostsPage.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+
+import appStyles from "../../App.module.css";
+
+
+function PostsPage() {
+  
+    return (
+        <Container>
+            <Row className="h-100">
+                <Col>Left Panel</Col>
+
+                <Col  sm={8}>
+                    <p>Popular profiles for mobile</p>
+
+                    
+                    <Container className={appStyles.Content}>
+                        Comments
+                    </Container>
+
+                </Col>
+
+                <Col  className="d-none d-lg-block p-0 p-lg-2">
+                    Popular profiles for desktop
+
+                </Col>
+            </Row>
+        </Container>
+    );
+
+}
+export default PostsPage;
