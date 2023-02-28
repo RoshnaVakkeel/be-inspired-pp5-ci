8909bd1b396370ac9edabb1e7bf8c5bd4ca138ec
RoshnaVakkeel <vakkeel.roshna@gmail.com>
2023-02-28 10:23:01 +0000

    Add three column view to PostPage

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -36,26 +36,33 @@ function PostPage() {
 
 
 
-        return (
-            <Row className="h-100">
-                <Col className="py-2 p-0 " md={11} xl={7}>
+    return (
+        <Container>
+            <Row>
+                <Col>Left Panel</Col>
 
+                <Col  sm={8}>
                     <p>Popular profiles for mobile</p>
 
-                    <Post 
-                    {...post.results[0]} 
-                    setPosts={setPost} 
-                    postPage/> 
+                    <Post
+                        {...post.results[0]}
+                        setPosts={setPost}
+                        postPage />
 
                     <Container className={appStyles.Content}>
                         Comments
                     </Container>
+
                 </Col>
-                <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
+
+                <Col  className="d-none d-lg-block p-0 p-lg-2">
                     Popular profiles for desktop
+
                 </Col>
             </Row>
-        );
-    }
+        </Container>
+    );
+
+}
 
 export default PostPage;
