dc9ea9904e0bb6bd564e9ad4d6e29d7fa96ff547
RoshnaVakkeel <vakkeel.roshna@gmail.com>
2023-03-01 10:39:17 +0000

    Create createform and styles for it

---
+++ src/pages/comments/CommentCreateForm.js
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+
+import styles from "../../styles/CommentCreateEditForm.module.css";
+import btnStyles from "../../styles/Button.module.css";
+import Avatar from "../../components/Avatar";
+import { axiosRes } from "../../api/axiosDefaults";
+
+function CommentCreateForm(props) {
+  const { post, setPost, setComments, profileImage, profile_id } = props;
+  const [content, setContent] = useState("");
+
+  const handleChange = (event) => {
+    setContent(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const { data } = await axiosRes.post("/comments/", {
+        content,
+        post,
+      });
+      setComments((prevComments) => ({
+        ...prevComments,
+        results: [data, ...prevComments.results],
+      }));
+      setPost((prevPost) => ({
+        results: [
+          {
+            ...prevPost.results[0],
+            comments_count: prevPost.results[0].comments_count + 1,
+          },
+        ],
+      }));
+      setContent("");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <Form
+      className={`${styles.CommentForm} mt-3`}
+      onSubmit={handleSubmit}
+    >
+      <Form.Group controlId='comment'>
+        <InputGroup className='align-items-center'>
+          <Link to={`/profiles/${profile_id}`}>
+            <Avatar src={profileImage} />
+          </Link>
+          <Form.Label srOnly>Enter a comment:</Form.Label>
+          <Form.Control 
+            placeholder='Add a comment...'
+            as='textarea'
+            name='comment'
+            value={content}
+            onChange={handleChange}
+            rows={2}
+          />
+        </InputGroup>
+      </Form.Group>
+      <button
+        className={`${btnStyles.CommentButton} d-block ml-auto`}
+        disabled={!content.trim()}
+        type="submit"
+      >
+        Post
+      </button>
+    </Form>
+  );
+}
+
+export default CommentCreateForm;
