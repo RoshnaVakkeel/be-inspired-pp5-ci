ca59b0fb3d4181c977beefe16e5880d89e15e5ab
RoshnaVakkeel <vakkeel.roshna@gmail.com>
2023-03-01 21:19:59 +0000

    Set alert on comment delete

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -8,6 +8,8 @@ import { DropdownMenu } from "../../components/DropdownMenu";
 import { useCurrentUser } from '../../contexts/CurrentUserContext';
 import { axiosRes } from '../../api/axiosDefaults';
 
+import FeedbackMsg from "../../components/Feedbackmsg";
+
 /**
  * Renders a selected Comment object from the API
  * Credit (variable, functions) - Moments walkthrough
@@ -27,11 +29,17 @@ const Comment = (props) => {
   const currentUser = useCurrentUser();
   const is_owner = currentUser?.username === owner;
   const [showEditForm, setShowEditForm] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
+  const [isDeleted, setIsDeleted] = useState(false);
 
   /** 
    * Handles deleting comment based on its id
+   * Reduces the total number of comments by 1
+   * Credit - Moments walkthrough, CI course material
     */
   const handleDelete = async () => {
+    setIsDeleted(true);
+    setTimeout(async () => {
     try {
       await axiosRes.delete(`/comments/${id}/`);
       setPost((prevPost) => ({
@@ -47,30 +55,43 @@ const Comment = (props) => {
         ...prevComments,
         results: prevComments.results.filter((comment) => comment.id !== id),
       }));
-    } catch (err) { }
+    } catch (err) { 
+        console.log(err)
+      }
+    }, 1500);
   };
 
-  return (
+  /**
+   * Alert Feedback messages upon post delete
+   * Credit - https://github.com/aleksandracodes/ci_pp5_snapfood/
+   */
+  return isDeleted ? (
+    <FeedbackMsg variant="info" message="Comment has been successfully deleted" />
+  ) : (
     <div className={styles.Comment}>
-      <hr />
+      {showAlert && (
+        <FeedbackMsg />
+      )}
+
       <Media>
-        <Link to={`/profiles/${profile_id}`}>
+        <Link to={`/profiles/${profile_id}`} className="my-3">
           <Avatar src={profile_image} />
         </Link>
-        <Media.Body className="align-self-left ml-2">
-          <span className={styles.Owner}>{owner}</span>
-          <span className={styles.Date}>|{updated_on}</span>
-          <span className={styles.DropdownDots}>
-            {/* Display the dropdown menu to owner of the comment
-                  to allow them to edit or delete it */}
-            {is_owner && !showEditForm && (
-              <DropdownMenu
-                handleEdit={() => setShowEditForm(true)}
-                handleDelete={handleDelete}
-              />
-            )}
-          </span>
-          {showEditForm ? (
+        <Media.Body className="align-self-center mb-4">
+          <div className={styles.CommentBox}>
+            <span className={styles.OwnerName}>{owner}</span>
+            <span className={styles.Date}> | {updated_on}</span>
+            <span className={styles.DropdownDots}>
+              {/* Display the dropdown menu for owner of the comment
+                  to either edit or delete it */}
+              {is_owner && !showEditForm && (
+                <DropdownMenu
+                  handleEdit={() => setShowEditForm(true)}
+                  handleDelete={handleDelete}
+                />
+              )}
+            </span>
+            {showEditForm ? (
               <CommentEditForm
                 id={id}
                 profile_id={profile_id}
@@ -78,15 +99,16 @@ const Comment = (props) => {
                 profileImage={profile_image}
                 setComments={setComments}
                 setShowEditForm={setShowEditForm}
+                setShowAlert={setShowAlert}
               />
             ) : (
               <p className="pr-2 pt-2">{content}</p>
             )}
+          </div>
         </Media.Body>
       </Media>
     </div>
   );
 };
-
 
 export default Comment
