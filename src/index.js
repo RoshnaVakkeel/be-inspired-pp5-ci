d9cc25c7bfba1ac92c7c41aa4e5c845911ae481c
RoshnaVakkeel <vakkeel.roshna@gmail.com>
2023-03-02 11:11:34 +0000

    Add ProfileDataProvider to index.js

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { CurrentUserProvider } from './contexts/CurrentUserContext';
+import { ProfileDataProvider } from './contexts/ProfileDataContext';
 
 
 ReactDOM.render(
     <Router>
       <CurrentUserProvider>
+      <ProfileDataProvider>
         <App />
+        </ProfileDataProvider>
       </CurrentUserProvider>
     </Router>,
   document.getElementById('root')
