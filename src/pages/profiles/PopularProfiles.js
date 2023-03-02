0f7aa6d715aa4a6647804d6e3974a18c2bd0b609
RoshnaVakkeel <vakkeel.roshna@gmail.com>
2023-03-02 00:05:10 +0000

    Add PopularProfiles component for different device

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -1,13 +1,69 @@
-import React from 'react'
-import { Container } from 'react-bootstrap'
-import appStyles from '../../App.module.css'
+import React, { useEffect, useState } from "react";
+import { Container } from "react-bootstrap";
+import { axiosReq } from "../../api/axiosDefaults";
+import appStyles from "../../App.module.css";
+import styles from '../../styles/PopularProfiles.module.css'
+import Asset from "../../components/Asset";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
-const PopularProfiles = () => {
-    return (
-        <Container className={appStyles.Content}>
-            <p>Most popular profiles</p>
-        </Container>
-    )
-}
+const PopularProfiles = ({ mobile }) => {
+  const [profileData, setProfileData] = useState({
+    pageProfile: { results: [] },
+    popularProfiles: { results: [] },
+  });
+  const { popularProfiles } = profileData;
+  const currentUser = useCurrentUser();
 
-export default PopularProfiles
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const { data } = await axiosReq.get(
+          "/profiles/?ordering=-followers_count"
+        );
+        setProfileData((prevState) => ({
+          ...prevState,
+          popularProfiles: data,
+        }));
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    handleMount();
+  }, [currentUser]);
+
+  return (
+    <Container
+      className={ `${appStyles.Content}
+      ${appStyles.ProfileContainer} 
+      ${mobile ? 
+          (
+              "d-xl-none text-center mb-4"
+          ) : (
+              styles.PopularProfiles
+          )
+      }`}
+    >
+      {popularProfiles.results.length ? (
+        <>
+          <p>Most followed profiles.</p>
+          {mobile ? (
+            <div className="d-flex justify-content-around">
+              {popularProfiles.results.slice(0, 4).map((profile) => (
+                <p key={profile.id}>{profile.owner}</p>
+              ))}
+            </div>
+          ) : (
+            popularProfiles.results.map((profile) => (
+              <p key={profile.id}>{profile.owner}</p>
+            ))
+          )}
+        </>
+      ) : (
+        <Asset spinner />
+      )}
+    </Container>
+  );
+};
+
+export default PopularProfiles;
