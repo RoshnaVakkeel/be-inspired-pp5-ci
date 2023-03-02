1d4a2f88ea01f9206c68a7a73e3832a2c5d86324
RoshnaVakkeel <vakkeel.roshna@gmail.com>
2023-03-02 11:10:54 +0000

    Add ProfileDataContext to provide profile access

---
+++ src/contexts/ProfileDataContext.js
@@ -0,0 +1,49 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { axiosReq } from "../api/axiosDefaults";
+import { useCurrentUser } from "../contexts/CurrentUserContext";
+
+const ProfileDataContext = createContext();
+const SetProfileDataContext = createContext();
+
+export const useProfileData = () => useContext(ProfileDataContext);
+export const useSetProfileData = () => useContext(SetProfileDataContext);
+
+/**
+ * To keep profile data to be in sync across all the components
+ * To access profile data in several places around our application
+ * Credit: CI's Moments Walkthrough
+ */
+
+export const ProfileDataProvider = ({ children }) => {
+  const [profileData, setProfileData] = useState({ 
+    popularProfiles: { results: [] },
+  });
+
+  const currentUser = useCurrentUser();
+
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const { data } = await axiosReq.get(
+          "/profiles/?ordering=-followers_count"
+        );
+        setProfileData((prevState) => ({
+          ...prevState,
+          popularProfiles: data,
+        }));
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    handleMount();
+  }, [currentUser]);
+
+  return (
+    <ProfileDataContext.Provider value={profileData}>
+      <SetProfileDataContext.Provider value={setProfileData}>
+        {children}
+      </SetProfileDataContext.Provider>
+    </ProfileDataContext.Provider>
+  );
+};
