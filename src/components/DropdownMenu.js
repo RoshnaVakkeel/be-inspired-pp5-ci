34e635de1ed746dbd759d730252cb44aa7aff73f
RoshnaVakkeel <vakkeel.roshna@gmail.com>
2023-03-04 23:14:38 +0000

    Set Dropdown menu for Profile editing options

--- src/components/DropdownMenu.js
+++ src/components/DropdownMenu.js
@@ -1,44 +1,86 @@
 import React from "react";
 import Dropdown from "react-bootstrap/Dropdown";
+import { useHistory } from "react-router-dom";
 import styles from "../styles/DropdownMenu.module.css"
 
-// The forwardRef is important!!
-// Dropdown needs access to the DOM node in order to position the Menu
+/**
+ * The forwardRef is important!!
+ * Dropdown needs access to the DOM node in order to position the Menu
+ * Credit - CI's Moments Walkthrough
+*/
+
 const ThreeDots = React.forwardRef(({ onClick }, ref) => (
-    <i
-      className="fas fa-ellipsis-v"
-      ref={ref}
-      onClick={(e) => {
-        e.preventDefault();
-        onClick(e);
-      }}
-    />
-  ));
-  
-  export const DropdownMenu = ({ handleEdit, handleDelete }) => {
-    return (
-      <Dropdown className="ml-auto" drop="left">
-        <Dropdown.Toggle as={ThreeDots}/>
-  
-        <Dropdown.Menu
-          className="text-center"
-          popperConfig={{ strategy: "fixed" }}
+  <i
+    className="fas fa-ellipsis-v"
+    ref={ref}
+    onClick={(e) => {
+      e.preventDefault();
+      onClick(e);
+    }}
+  />
+));
+
+export const DropdownMenu = ({ handleEdit, handleDelete }) => {
+  return (
+    <Dropdown className="ml-auto" drop="left">
+      <Dropdown.Toggle as={ThreeDots} />
+
+      <Dropdown.Menu
+        className="text-center"
+        popperConfig={{ strategy: "fixed" }}
+      >
+        <Dropdown.Item
+          className={styles.DropdownItem}
+          onClick={handleEdit}
+          aria-label="edit"
         >
-          <Dropdown.Item
-            className={styles.DropdownItem}
-            onClick={handleEdit}
-            aria-label="edit"
-          >
-            <i className="fas fa-edit" />
-          </Dropdown.Item>
-          <Dropdown.Item
-            className={styles.DropdownItem}
-            onClick={handleDelete}
-            aria-label="delete"
-          >
-            <i className="fas fa-trash-alt" />
-          </Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
-    );
-  };
+          <i className="fas fa-edit" />
+        </Dropdown.Item>
+        <Dropdown.Item
+          className={styles.DropdownItem}
+          onClick={handleDelete}
+          aria-label="delete"
+        >
+          <i className="fas fa-trash-alt" />
+        </Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+};
+
+/**
+ * Profile page Dropdown menu
+ * Shows icons for edit profile & change password
+ * Makes a request to fetch profile data based on the profile id
+ * Credit - CI's Moments Walkthrough
+*/
+export const ProfileEditDropdown = ({ id }) => {
+  const history = useHistory();
+  return (
+    <Dropdown className={`ml-auto px-3 ${styles.Absolute}`} drop="left">
+      <Dropdown.Toggle as={ThreeDots} />
+      <Dropdown.Menu>
+        <Dropdown.Item
+          onClick={() => history.push(`/profiles/${id}/edit`)}
+          aria-label="edit-profile"
+        >
+          <i className="fas fa-edit" /> edit profile
+        </Dropdown.Item>
+        <Dropdown.Item
+          onClick={() => history.push(`/profiles/${id}/edit/username`)}
+          aria-label="edit-username"
+        >
+          <i className="far fa-id-card" />
+          change username
+        </Dropdown.Item>
+        <Dropdown.Item
+          onClick={() => history.push(`/profiles/${id}/edit/password`)}
+          aria-label="edit-password"
+        >
+          <i className="fas fa-key" />
+          change password
+        </Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+};
